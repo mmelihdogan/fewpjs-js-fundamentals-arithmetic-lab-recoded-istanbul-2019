7a9d070b86ffd41cbef8707d2cec8bd7f12df974
mmelihdogan <doganmmelih@gmail.com>
2020-02-02 14:34:13 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+let random;
+let multiply;
+let mod, max;
+
+
+multiply = num1 * num2;
+
+random = Math.random();
+
+mod = 9 % 5;
+
+max = Math.max(1,2,3,20);
